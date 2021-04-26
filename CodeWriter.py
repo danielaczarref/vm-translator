92b23d233f98ecb92f38e6880d532f255e28414e
André Cardoso <andre.cardoso@lsdi.ufma.br>
2021-04-26 14:13:25 -0300

    Implementando writePush

--- CodeWriter.py
+++ CodeWriter.py
@@ -2,21 +2,62 @@ class CodeWriter:
 
 
     def __init__ (self, outputFileName):
-        pass
+        self.output = open(outputFileName, "w")
+        self.moduleName = outputFileName.split(".")[0]
 
+        print(self.moduleName)
+        # print("pop {} {}".format(replaced, indice), file=self.output)
+
+    def getSegmentPointer(self, segment, index):
+        if(segment == "local"): return "LCL"
+        elif(segment == "argument") : return "ARG"
+        elif(segment in ["this", "that"]) : return segment.upper()
+        elif(segment == "temp") : return "R{}".format(5+index)
+        elif(segment == "pointer") : return "R{}".format(3+index)
+        elif(segment == "static") : return "{}.{}".format(self.moduleName, index)
+        else:
+            print("error")
+
+
+    def write(self, text):
+        print("{}".format(text), file=self.output)
 
     def writeArithmetic (self):
-        print("writeArithmetic() not implemented yet!")
+            print("writeArithmetic() not implemented yet!")
 
     
-    def writePush(self):
-        print("writePush() not implemented yet!")
+    def writePush(self, segment, index):
+        if (segment == "constant"):
+            self.write("@{} // push {} {}".format(index, segment, index))
+            self.write("D=A")
+            self.write("@SP")
+            self.write("A=M")
+            self.write("M=D")
+            self.write("@SP")
+            self.write("M=M+1")
+        elif(segment in ["static", "temp", "pointer"]):
+            self.write("@{} // push {} {}".format(self.getSegmentPointer(segment, index), segment, index))
+            self.write("D=M")
+            self.write("@SP")
+            self.write("A=M")
+            self.write("M=D")
+            self.write("@SP")
+            self.write("M=M+1")
+        elif(segment in ["local", "argument", "this", "that"]):
+            self.write("@{} // push {} {}".format(self.getSegmentPointer(segment, index), segment, index))
+            self.write("D=M")
+            self.write("@%d".format(index))
+            self.write("A=D+A")
+            self.write("D=M")
+            self.write("@SP")
+            self.write("A=M")
+            self.write("M=D")
+            self.write("@SP")
+            self.write("M=M+1")
 
-
-    def writePop(self):
+    def writePop(self, segment, index):
         print("writePop() not implemented yet!")
 
     
     def close(self):
-        pass
-    
+        self.output.close()
