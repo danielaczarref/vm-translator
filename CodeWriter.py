c5c16e00f0bd2b00e392e0f0f8737190db50fef7
Daniela Carvalho <daniela.czarref@gmail.com>
2021-04-26 01:51:17 -0300

    D -CodeWriter- desenvolvimento da classe CodeWriter com as funcoes de writeArithmetic, writePush, writePop e close

---
+++ CodeWriter.py
@@ -0,0 +1,22 @@
+class CodeWriter:
+
+
+    def __init__ (self, outputFileName):
+        pass
+
+
+    def writeArithmetic (self):
+        print("writeArithmetic() not implemented yet!")
+
+    
+    def writePush(self):
+        print("writePush() not implemented yet!")
+
+
+    def writePop(self):
+        print("writePop() not implemented yet!")
+
+    
+    def close(self):
+        pass
+    
